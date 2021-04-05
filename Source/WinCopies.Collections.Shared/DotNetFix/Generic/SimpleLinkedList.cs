ec89c4fa2b2de4d0c81a1faf0f86b70a139df797
pierresprim <wincopies@wincopies.com>
2021-04-05 10:57:09 +0200

    Merge SimpleLinkedList types files

--- Source/WinCopies.Collections.Shared/DotNetFix/Generic/SimpleLinkedList.cs
+++ Source/WinCopies.Collections.Shared/DotNetFix/Generic/SimpleLinkedList.cs
@@ -18,8 +18,129 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using static WinCopies.Collections.ThrowHelper;
+
 namespace WinCopies.Collections.DotNetFix.Generic
 {
+    public abstract class SimpleLinkedList<T> : SimpleLinkedListBase, ISimpleLinkedList<T>
+    {
+        private uint _count = 0;
+
+        public sealed override bool IsReadOnly => false;
+
+        protected internal SimpleLinkedListNode<T> FirstItem { get; private set; }
+
+        public sealed override uint Count => _count;
+
+        protected void Add(in T item)
+        {
+            if (IsReadOnly)
+
+                throw GetReadOnlyListOrCollectionException();
+
+            FirstItem = AddItem(item, out bool actionAfter);
+
+            _count++;
+
+            if (actionAfter)
+
+                OnItemAdded();
+        }
+
+        protected abstract SimpleLinkedListNode<T> AddItem(T item, out bool actionAfter);
+
+        protected abstract void OnItemAdded();
+
+        private T OnRemove()
+        {
+            SimpleLinkedListNode<T> firstItem = FirstItem;
+
+            T result = firstItem.Value;
+
+            FirstItem = RemoveItem();
+
+            firstItem.Clear();
+
+            _count--;
+
+            return result;
+        }
+
+        protected T Remove()
+        {
+            if (IsReadOnly)
+
+                throw GetReadOnlyListOrCollectionException();
+
+#if !WinCopies3
+            ThrowIfEmpty
+#else
+            ThrowIfEmptyListOrCollection
+#endif
+            (this);
+
+            return OnRemove();
+        }
+
+        protected bool TryRemove(out T result)
+        {
+            if (IsReadOnly || Count == 0)
+            {
+                result = default;
+
+                return false;
+            }
+
+            result = OnRemove();
+
+            return true;
+        }
+
+        protected abstract SimpleLinkedListNode<T> RemoveItem();
+
+#if !WinCopies3
+        public
+#else
+        protected
+#endif
+            sealed override void ClearItems()
+        {
+            SimpleLinkedListNode<T> node, temp;
+            node = FirstItem;
+
+            while (node != null)
+            {
+                temp = node.Next;
+
+                node.Clear();
+
+                node = temp;
+            }
+
+            FirstItem = null;
+
+            _count = 0;
+        }
+
+        protected T _Peek() => FirstItem.Value;
+
+        public T Peek() => _count > 0 ? _Peek() : throw GetEmptyListOrCollectionException();
+
+        public bool TryPeek(out T result)
+        {
+            if (_count > 0)
+            {
+                result = _Peek();
+
+                return true;
+            }
+
+            result = default;
+
+            return false;
+        }
+    }
+
     public interface ISimpleLinkedListNode<T>
 #if WinCopies3
         : ISimpleLinkedListNode
