92a94fe38efb4bf6ecc5add5101cc7cfab54e864
pierresprim <49271108+pierresprim@users.noreply.github.com>
2023-03-29 07:20:08 +0200

    Update Collections.cs

--- Source/Shared/WinCopies.Collections.Shared/Collections.cs
+++ Source/Shared/WinCopies.Collections.Shared/Collections.cs
@@ -15,15 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with the WinCopies Framework.  If not, see <https://www.gnu.org/licenses/>. */
 
-#if !WinCopies3
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-
-using IDisposable = WinCopies.Util.DotNetFix.IDisposable;
-#endif
-
 namespace WinCopies.Collections
 {
     /// <summary>
@@ -64,7 +55,8 @@ namespace WinCopies.Collections
 #if CS8
             ?
 #endif
-            this[int index] { get; }
+            this[int index]
+        { get; }
     }
 
     public interface IIndexableW
@@ -73,7 +65,8 @@ namespace WinCopies.Collections
 #if CS8
             ?
 #endif
-            this[int index] { set; }
+            this[int index]
+        { set; }
     }
 
     public interface IIndexable : IIndexableR, IIndexableW
@@ -131,7 +124,6 @@ namespace WinCopies.Collections
         public interface IIndexableR<out T> : IIndexableR
         {
             new T this[int index] { get; }
-
 #if CS8
             object IIndexableR.this[int index] => this[index];
 #endif
@@ -140,7 +132,6 @@ namespace WinCopies.Collections
         public interface IIndexableW<in T> : IIndexableW
         {
             new T this[int index] { set; }
-
 #if CS8
             object IIndexableW.this[int index] { set => this[index] = (T)value; }
 #endif
@@ -154,7 +145,6 @@ namespace WinCopies.Collections
         public interface IUIntIndexableR<out T> : IUIntIndexableR
         {
             new T this[uint index] { get; }
-
 #if CS8
             object IUIntIndexableR.this[uint index] => this[index];
 #endif
@@ -163,7 +153,6 @@ namespace WinCopies.Collections
         public interface IUIntIndexableW<in T> : IUIntIndexableW
         {
             new T this[uint index] { set; }
-
 #if CS8
             object IUIntIndexableW.this[uint index] { set => this[index] = (T)value; }
 #endif
@@ -177,7 +166,6 @@ namespace WinCopies.Collections
         public interface ILongIndexableR<out T> : ILongIndexableR
         {
             new T this[long index] { get; }
-
 #if CS8
             object ILongIndexableR.this[long index] => this[index];
 #endif
@@ -186,7 +174,6 @@ namespace WinCopies.Collections
         public interface ILongIndexableW<in T> : ILongIndexableW
         {
             new T this[long index] { set; }
-
 #if CS8
             object ILongIndexableW.this[long index] { set => this[index] = (T)value; }
 #endif
@@ -200,7 +187,6 @@ namespace WinCopies.Collections
         public interface IULongIndexableR<out T> : IULongIndexableR
         {
             new T this[ulong index] { get; }
-
 #if CS8
             object IULongIndexableR.this[ulong index] => this[index];
 #endif
@@ -209,7 +195,6 @@ namespace WinCopies.Collections
         public interface IULongIndexableW<in T> : IULongIndexableW
         {
             new T this[ulong index] { set; }
-
 #if CS8
             object IULongIndexableW.this[ulong index] { set => this[index] = (T)value; }
 #endif
@@ -221,23 +206,7 @@ namespace WinCopies.Collections
         }
     }
 
-    public interface IEnumeratorInfo :
-#if !WinCopies3
-System.Collections.IEnumerator
-    {
-        bool? IsResetSupported { get; }
-
-        bool IsStarted { get; }
-
-        bool IsCompleted { get; }
-    }
-    
-    public interface IDisposableEnumeratorInfo : IEnumeratorInfo, WinCopies.Util.DotNetFix.IDisposable
-    {
-        // Left empty.
-    }
-#else
-        DotNetFix.IEnumerator, DotNetFix.IEnumeratorInfo
+    public interface IEnumeratorInfo : DotNetFix.IEnumerator, DotNetFix.IEnumeratorInfo
     {
         // Left empty.
     }
@@ -246,196 +215,4 @@ System.Collections.IEnumerator
     {
         // Left empty.
     }
-#endif
-
-#if !WinCopies3
-    [Obsolete("This type has been replaced by the types in the WinCopies.Collections.DotNetFix namespace and will be removed in later versions.")]
-    public interface IUIntIndexedCollection
-    {
-        object this[uint index] { get; }
-
-        uint Count { get; }
-    }
-
-    [Obsolete("This type has been replaced by the types in the WinCopies.Collections.DotNetFix namespace and will be removed in later versions.")]
-    public interface IUIntIndexedCollection<T> : IUIntIndexedCollection
-    {
-        T this[uint index] { get; }
-    }
-
-    [Obsolete("This type has been replaced by the types in the WinCopies.Collections.DotNetFix namespace and will be removed in later versions.")]
-    public abstract class UIntIndexedCollectionEnumeratorBase : WinCopies.Util.DotNetFix.IDisposable
-    {
-        protected internal IUIntIndexedCollection UIntIndexedCollection { get; private set; }
-        protected internal uint? Index { get; set; } = null;
-        protected internal Func<bool> MoveNextMethod { get; set; }
-
-        public UIntIndexedCollectionEnumeratorBase(IUIntIndexedCollection uintIndexedCollection)
-        {
-            UIntIndexedCollection = uintIndexedCollection;
-
-            MoveNextMethod = () => UIntIndexedCollectionEnumerator.MoveNextMethod(this);
-        }
-
-    #region IDisposable Support
-        public bool IsDisposed { get; private set; } = false;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!IsDisposed)
-            {
-                if (disposing)
-                {
-                    UIntIndexedCollection = null;
-
-                    Index = null;
-                }
-
-                IsDisposed = true;
-            }
-        }
-
-        public void Dispose() => Dispose(true);
-    #endregion
-
-        public virtual bool MoveNext() => MoveNextMethod();
-
-        public virtual void Reset()
-        {
-            Index = null;
-
-            MoveNextMethod = () => UIntIndexedCollectionEnumerator.MoveNextMethod(this);
-        }
-    }
-
-    [Obsolete("This type has been replaced by the types in the WinCopies.Collections.DotNetFix namespace and will be removed in later versions.")]
-    public sealed class UIntIndexedCollectionEnumerator : UIntIndexedCollectionEnumeratorBase, System.Collections.IEnumerator
-    {
-        public static Func<UIntIndexedCollectionEnumeratorBase, bool> MoveNextMethod => (UIntIndexedCollectionEnumeratorBase e) =>
-        {
-            if (e.UIntIndexedCollection.Count > 0)
-            {
-                e.Index = 0;
-
-                e.MoveNextMethod = () =>
-                {
-                    if (e.Index < e.UIntIndexedCollection.Count - 1)
-                    {
-                        e.Index++;
-
-                        return true;
-                    }
-
-                    else return false;
-                };
-
-                return true;
-            }
-
-            else return false;
-        };
-
-        public object Current
-        {
-            get
-            {
-                Debug.Assert(Index.HasValue, "_index does not have value.");
-
-                return UIntIndexedCollection[Index.Value];
-            }
-        }
-
-        public UIntIndexedCollectionEnumerator(IUIntIndexedCollection uintIndexedCollection) : base(uintIndexedCollection) { }
-    }
-
-    [Obsolete("This type has been replaced by the types in the WinCopies.Collections.DotNetFix namespace and will be removed in later versions.")]
-    public sealed class UIntIndexedCollectionEnumerator<T> : UIntIndexedCollectionEnumeratorBase, System.Collections.Generic.IEnumerator<T>
-    {
-        public T Current
-        {
-            get
-            {
-                Debug.Assert(Index.HasValue, "_index does not have value.");
-
-                return ((IUIntIndexedCollection<T>)UIntIndexedCollection)[Index.Value];
-            }
-        }
-
-        object System.Collections.IEnumerator.Current => Current;
-
-        public UIntIndexedCollectionEnumerator(IUIntIndexedCollection<T> uintIndexedCollection) : base(uintIndexedCollection) { }
-    }
-#endif
 }
-
-//public interface IList : System.Collections. IList, ICollection, IEnumerable
-
-//{
-
-
-
-//}
-
-//public interface IList<T> : System.Collections.Generic.IList<T>, System.Collections.Generic.IReadOnlyList<T>, ICollection, IList
-
-//{
-
-//    new void Clear();
-
-//    new int Count { get; }
-
-//    //new System.Collections.Generic.IEnumerator<T> GetEnumerator();
-
-//    //new bool IsReadOnly { get; }
-
-//    new void RemoveAt(int index);
-
-//    new T this[int index] { get; set; }
-
-//}
-
-//public interface IReadOnlyList<T> : System.Collections.Generic.IList<T>, System.Collections.Generic.IReadOnlyList<T>, ICollection, IList
-
-//{
-
-//    new void Clear();
-
-//    new int Count { get; }
-
-//    //new System.Collections.Generic.IEnumerator<T> GetEnumerator();
-
-//    //new bool IsReadOnly { get; }
-
-//    new void RemoveAt(int index);
-
-//    new T this[int index] { get; set; }
-
-//}
-
-//public class ReadOnlyCollection<T> : System.Collections.ObjectModel.ReadOnlyCollection<T>, IReadOnlyList<T>
-
-//{
-
-//    public ReadOnlyCollection(System.Collections.Generic.IList<T> list) : base(list) { } 
-
-//    T IReadOnlyList<T>.this[int index] { get => this[index]; /*set => throw new NotSupportedException("This collection is read-only.") ;*/ } 
-
-//    // int System.Collections.Generic.IReadOnlyCollection<T>.Count => Count ; 
-
-//    // bool IReadOnlyList<T>.IsReadOnly => true ; 
-
-//    //void IReadOnlyList<T>.Clear() => throw new NotSupportedException("This collection is read-only.") ; 
-
-//    // System.Collections.Generic.IEnumerator<T> System.Collections.Generic.IReadOnlyCollection<T>.GetEnumerator() => throw new NotImplementedException();
-
-//    //void IReadOnlyList<T>.RemoveAt(int index) => throw new NotSupportedException("This collection is read-only.") ;
-
-//}
-
-//public interface System.Collections.Generic.ICollection<T, U> : System.Collections.Generic.ICollection<U> where T : U
-
-//{
-
-
-
-//}
